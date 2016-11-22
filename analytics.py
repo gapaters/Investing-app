b91662e872cd6530c98190373562892b62bb27c6
Gareth Paterson <gapaters@users.noreply.github.com>
2016-11-22 15:32:56 -0700

    Add divide by zero error checking

--- analytics.py
+++ analytics.py
@@ -25,16 +25,29 @@ while True:
     dividends_payable = input('Enter the dividends payable: ')
     dividends_payable = int(dividends_payable)
 
-    price_per_book = (
-        number_of_shares * share_price) / (total_assets - total_liabilities)
-    price_per_earnings = (number_of_shares * share_price) / net_income
-    dividend_percentage = dividends_payable / (number_of_shares * share_price)
-    dividend_safety = dividends_payable / net_income
+    try:
+        price_per_book = (number_of_shares * share_price) / (
+            total_assets - total_liabilities)
+    except ZeroDivisionError:
+        price_per_book = 'undefined'
+    try:
+        price_per_earnings = (number_of_shares * share_price) / net_income
+    except ZeroDivisionError:
+        price_per_earnings = 'undefined'
+    try:
+        dividend_percentage = dividends_payable / (
+            number_of_shares * share_price)
+    except ZeroDivisionError:
+        dividend_percentage = 'undefined'
+    try:
+        dividend_safety = dividends_payable / net_income
+    except ZeroDivisionError:
+        dividend_safety = 'undefined'
 
     while True:
-        print('The price/book is ', price_per_book)
+        print('The price/book is', price_per_book)
         print('The price/earnings is', price_per_earnings)
-        print('The dividends percentage is ', dividend_percentage)
+        print('The dividends percentage is', dividend_percentage)
         print('The dividend safety is', dividend_safety)
         input('Press any key to quit')
         break
