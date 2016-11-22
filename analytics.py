aadc380d3afb8017c808c1247ccee9cda7387578
Gareth Paterson <gapaters@users.noreply.github.com>
2016-11-22 15:19:40 -0700

    Fix float input conversion

--- analytics.py
+++ analytics.py
@@ -1,19 +1,17 @@
 #!/usr/bin/env python3.5
 
-# import os
-
 while True:
-    number_of_shares = input('Enter the number of shares: ')
+    number_of_shares = input('Enter the total number of shares floated: ')
     number_of_shares = int(number_of_shares)
 
     share_price = input('Enter the share price: ')
-    share_price = int(share_price)
+    share_price = float(share_price)
 
     fifty_two_week_high = input('Enter the 52 week high: ')
-    fifty_two_week_high = int(fifty_two_week_high)
+    fifty_two_week_high = float(fifty_two_week_high)
 
     fifty_two_week_low = input('Enter the 52 week low: ')
-    fifty_two_week_low = int(fifty_two_week_low)
+    fifty_two_week_low = float(fifty_two_week_low)
 
     total_assets = input('Enter the total assets: ')
     total_assets = int(total_assets)
@@ -29,13 +27,9 @@ while True:
 
     price_per_book = (
         number_of_shares * share_price) / (total_assets - total_liabilities)
-    # print(number_of_shares)
     price_per_earnings = (number_of_shares * share_price) / net_income
-    # print(number_of_shares)
     dividend_percentage = dividends_payable / (number_of_shares * share_price)
-    # print(number_of_shares)
     dividend_safety = dividends_payable / net_income
-    # print(number_of_shares)
 
     while True:
         print('The price/book is ', price_per_book)
