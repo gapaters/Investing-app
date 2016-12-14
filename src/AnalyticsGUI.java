13a7b4136fc7362ed896af719a6d5a71a5cab24a
Gareth Paterson <gapaters@users.noreply.github.com>
2016-12-13 18:35:07 -0700

    Add 52 week price gauge in seperate window

--- src/AnalyticsGUI.java
+++ src/AnalyticsGUI.java
@@ -1,11 +1,26 @@
+import com.sun.javafx.application.PlatformImpl;
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.embed.swing.JFXPanel;
+import javafx.scene.Scene;
+import javafx.scene.chart.BarChart;
+import javafx.scene.chart.CategoryAxis;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
 import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.*;
+import java.awt.event.*;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 /**
  * Created by gareth on 06/12/16.
  */
-public class AnalyticsGUI {
+public class AnalyticsGUI extends JPanel{
     private JPanel mainWindow;
     private JLabel numberOfSharesLabel;
     private JTextField numberOfSharesInput;
@@ -44,9 +59,11 @@ public class AnalyticsGUI {
     private JLabel dividendsSafetyResult;
 
     Analytics analytics = new Analytics();
+    static JFrame frame;
+    JFXPanel jfxPanel;
 
     public static void main(String[] args) {
-        JFrame frame = new JFrame("AnalyticsGUI");
+        frame = new JFrame("AnalyticsGUI");
         frame.setContentPane(new AnalyticsGUI().mainWindow);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
@@ -105,7 +122,74 @@ public class AnalyticsGUI {
                 pricePerEarningsResult.setText(Double.toString(analytics.getPricePerEarnings()));
                 dividendPercentageResult.setText(Double.toString(analytics.getDividendPercentage()));
                 dividendsSafetyResult.setText(Double.toString(analytics.getdividendSafety()));
+
+                SwingUtilities.invokeLater(new Runnable() {
+                    @Override
+                    public void run() {
+                        PriceGaugeBarGraph barGraph = new PriceGaugeBarGraph();
+                        barGraph.setVisible(true);
+                    }
+                });
             }
         });
+    }
+
+
+    class PriceGaugeBarGraph extends JFrame {
+        JFXPanel jfxPanel;
+
+        public PriceGaugeBarGraph(){
+            initSwingComponents();
+            initFxComponents();
+        }
+
+        private void initSwingComponents(){
+            JPanel graphPanel = new JPanel(new BorderLayout());
+            jfxPanel = new JFXPanel();
+            graphPanel.add(jfxPanel, BorderLayout.CENTER);
+            JLabel title = new JLabel("test label");
+            graphPanel.add(title, BorderLayout.NORTH);
+
+            this.add(graphPanel);
+            //this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            this.setSize(800,400);
+        }
+
+        private void initFxComponents(){
+            Platform.runLater(new Runnable() {
+                @Override
+                public void run() {
+                    GridPane grid = new GridPane();
+                    Scene scene = new Scene(grid, 800,400);
+
+                    final CategoryAxis xAxis = new CategoryAxis();
+                    final NumberAxis yAxis = new NumberAxis();
+                    final BarChart<String, Number> bc =
+                            new BarChart<String, Number>(xAxis, yAxis);
+                    bc.setTitle("52 Week Price Gauge");
+                    xAxis.setLabel("Price Point");
+                    yAxis.setLabel("Price");
+
+                    XYChart.Series series1 = new XYChart.Series();
+                    DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd");
+                    Date date = new Date();
+                    series1.setName(dateFormat.format(date));
+
+                    XYChart.Data<String, Number> dataHigh = new XYChart.Data<String, Number>("52 Week High", analytics.getFiftyTwoWeekHigh());
+                    XYChart.Data<String, Number> dataCurrent = new XYChart.Data<String, Number>("Current Price", analytics.getSharePrice());
+                    XYChart.Data<String, Number> dataLow = new XYChart.Data<String, Number>("52 Week Low", analytics.getFiftyTwoWeekLow());
+                    series1.getData().add(dataHigh);
+                    series1.getData().add(dataCurrent);
+                    series1.getData().add(dataLow);
+
+                    bc.getData().addAll(series1);
+                    grid.add(bc,0,0);
+                    jfxPanel.setScene(scene);
+                    dataHigh.getNode().setStyle("-fx-bar-fill: red;");
+                    dataCurrent.getNode().setStyle("-fx-bar-fill: green;");
+                    dataLow.getNode().setStyle("-fx-bar-fill: blue;");
+                }
+            });
+        }
     }
 }
